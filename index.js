1e54e652e0ffec51d4b046fc3c066a980e5fe34b
Alex Berns <tagger94@gmail.com>
2016-09-17 21:01:31 +0000

    producer is broken but added admin stuff

--- index.js
+++ index.js
@@ -4,72 +4,34 @@ var io = require('socket.io')(http);
 var patientList = require('./patient.json');
 var monitorList = require('./monitor.json');
 
-//console.log(patientList);
-
 var counter = 0;
 
-var PB = {
-    '0094': {
-        name: 'Alex Berns',
-        pid: '0094',
-        last_heart: 0,
-        last_gluc: 0,
-        last_oxyg: 0,
-        crit_heart: 90,
-        crit_gluc: 30,
-        crit_oxyg: 60
-    },
-    
-    '0000':{
-        name: 'Pepsi Sam',
-        pid: '0000',
-        last_heart: 0,
-        last_gluc: 0,
-        last_oxyg: 0,
-        crit_heart: 80,
-        crit_gluc: 70,
-        crit_oxyg: 55
-    }
-}
+var patientRooms = {};
 
-var MB = {
-    ps123456: {
-        mid: 'ps123456',
-        pid: '0094',
-        type: 'heart'
-    },
-    
-    ps000000: {
-        mid: 'ps000000',
-        pid:'0000',
-        type: 'heart'
-    }, 
-    
-    gl111222:{
-        mid: 'gl111222',
-        pid: '0094',
-        type: 'gluc'
-    }
-}
-
-var openRooms = {};
+var adminRoom = io.of('/admin');
+var monitorRoom = io.of('/moniter');
 
 app.get('/', function(req, res) {
-    if(counter % 2 == 0){
+    if (counter % 2 == 0) {
         res.sendFile(__dirname + '/producer.html');
-    } else {
+    }
+    else {
         res.sendFile(__dirname + '/consumer.html');
     }
     counter++;
-    
+
 });
 
 app.get('/consumer', function(req, res) {
-        res.sendFile(__dirname + '/consumer.html');
+    res.sendFile(__dirname + '/consumer.html');
 });
 
 app.get('/producer', function(req, res) {
-        res.sendFile(__dirname + '/producer.html');
+    res.sendFile(__dirname + '/producer.html');
+});
+
+app.get('/admin', function(req, res) {
+    res.sendFile(__dirname + '/admin.html');
 });
 
 http.listen(process.env.PORT, function() {
@@ -77,63 +39,108 @@ http.listen(process.env.PORT, function() {
 });
 
 //CODE
-
-io.on('connection', function(socket) {
-    console.log('Connected to producer');
-    socket.on('patient update', recievePatientUpdate);
-    
+/********
+Producer Methods
+*********/
+monitorRoom.on('connection', function(socket) {
+    console.log('Connected made to monitor');
 });
+
+monitorRoom.on('patient update', recievePatientUpdate);
 
 function recievePatientUpdate(update) {
     //Patient Data is recieved
     //console.log(update.mid + ': ' + update.data);
-    
+
     var mid = update.mid;
-    var mType = mid.slice(0,2);
+    var mType = mid.slice(0, 2);
     var pid = monitorList[mid].pid;
-    
+
     console.log("Update from " + mid + ' for ' + pid + ': ' + update.data);
-    
+
     //Send to database for updating
-    if(mType == 'hb'){
+    if (mType == 'hb') {
         patientList[pid].last_heart = update.data;
-        
+
         //Check if alert is needed
-        if(update.data > patientList[pid].crit_high_heart || update.data < patientList[pid].crit_low_heart){
-            sendAlert(pid, mType, update);
-        }
-    } else if(mType == 'gl') {
-        patientList[pid].last_gluc = update.data;
-        
-        //Check if alert is needed
-        if(update.data > patientList[pid].crit_gluc){
-            sendAlert(pid, mType, update);
-        }
-    } else if(mType == 'wb'){
-        patientList[pid].last_oxyg = update.data;
-        
-        //Check if alert is needed
-        if(update.data > patientList[pid].crit_oxyg){
+        if (update.data > patientList[pid].crit_high_heart || update.data < patientList[pid].crit_low_heart) {
             sendAlert(pid, mType, update);
         }
     }
+    else if (mType == 'gl') {
+        patientList[pid].last_gluc = update.data;
+
+        //Check if alert is needed
+        if (update.data > patientList[pid].crit_gluc) {
+            sendAlert(pid, mType, update);
+        }
+    }
+    else if (mType == 'wb') {
+        patientList[pid].last_oxyg = update.data;
+
+        //Check if alert is needed
+        if (update.data > patientList[pid].crit_oxyg) {
+            sendAlert(pid, mType, update);
+        }
+    }
+};
+
+/********
+Consumer Methods
+*********/
+
+function createNewPatientRoom(pid) {
+    //Check if room is already open
+    if (!patientRooms.hasOwnProperty(pid)) {
+        //open room
+        patientRooms[pid] = io.of('/' + pid);
+        console.log('opening room: ' + pid);
+    }
+    
+    patientRooms[pid].on('connection', function() {
+        console.log("Connected to patient room");
+    })
+    
+    patientRooms[pid].on('disconnect', function() {
+        console.log("disconnected to patient room");
+    })
 }
 
 function sendAlert(pid, type, update) {
     //Prepare to send Alert
-    
-    //Check if room is already open
-    if(!openRooms.hasOwnProperty(pid)){
-        //open room
-        openRooms[pid] = io.of('/' + pid);
-        console.log('opening room: ' + pid);
-    }
-    
+    createNewPatientRoom(pid);
+
     var message = "ALERT: " + pid + " has a critical status for " + type;
     console.log(message);
-    
+
     //Send to socket
-    openRooms[pid].emit('alert', message);
+    patientRooms[pid].emit('alert', message);
+}
+
+/********
+Admin Methods
+*********/
+
+adminRoom.on('connection', function(socket) {
+    console.log('Admin has connected');
+
+    // send information back to admin
+
+});
+
+adminRoom.on('disconnect', function(socket) {
+    console.log('Admin has disconnected');
+});
+
+adminRoom.on('request patient list', sendPatientListToAdmin);
+adminRoom.on('request moniter list', sendMoniterListToAdmin);
+
+function sendPatientListToAdmin() {
+    adminRoom.emit('patient list', patientList);
+}
+
+function sendMoniterListToAdmin() {
+
 }
 
 
