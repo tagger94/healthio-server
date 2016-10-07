9db75ee50290356ea833255ed3e0330503184013
Jordan <jdkauffman11@gmail.com>
2016-10-07 12:49:47 -0500

    Can now update specific info within the database

--- Firebase.js
+++ Firebase.js
@@ -47,26 +47,10 @@ function retrieveUserData(patientID, key, onCall){
 }
 
 
-//This function below is the firebase example of updating values in the DB
+function updatePatientInfo(patientID, value, updatedInfo) {
 
-//function writeNewPost(uid, username, picture, title, body) {
-//    // A post entry.
-//    var postData = {
-//        author: username,
-//        uid: uid,
-//        body: body,
-//        title: title,
-//        starCount: 0,
-//        authorPic: picture
-//    };
-//
-//    // Get a key for a new Post.
-//    var newPostKey = firebase.database().ref().child('posts').push().key;
-//
-//    // Write the new post's data simultaneously in the posts list and the user's post list.
-//    var updates = {};
-//    updates['/posts/' + newPostKey] = postData;
-//    updates['/user-posts/' + uid + '/' + newPostKey] = postData;
-//
-//    return firebase.database().ref().update(updates);
-//}
+    var updates = {};
+    updates['Patients/' + patientID + '/' + value] = updatedInfo;
+
+    return database.ref().update(updates);
+}
