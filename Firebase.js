8f761625797b1cd27061053b8144d88e9ef19540
Alex Berns <tagger94@gmail.com>
2016-10-11 16:30:38 +0000

    test db fix

--- Firebase.js
+++ Firebase.js
@@ -16,48 +16,48 @@ firebase.initializeApp(config);
 var database = firebase.database();
 
 // Writes data to the root of the patient. I am hoping that this works and doesn't ruin the object
-function createPatient(patientID, city, state, street, zip, name, dob, gender, height, weight, insurance, location, provider ) {
+function createPatient(patientID, city, state, street, zip, name, dob, gender, height, weight, insurance, location, provider) {
 
     var addressDict = {
-        "City" : city,
-        "State" : state,
-        "Street" : street,
-        "Zip" : zip
+        "City": city,
+        "State": state,
+        "Street": street,
+        "Zip": zip
     };
 
     database.ref('Patients/' + patientID + '/').set({
-        Address : addressDict,
-        Name : name,
-        DOB : dob,
-        Gender : gender,
-        Height : height,
-        Weight : weight,
-        Insurance : insurance,
-        Location : location,
-        Provider : provider
+        Address: addressDict,
+        Name: name,
+        DOB: dob,
+        Gender: gender,
+        Height: height,
+        Weight: weight,
+        Insurance: insurance,
+        Location: location,
+        Provider: provider
     });
 }
 
 
 // Retrieves data from the Database. key will need to be a path if it is going into a dictionary within the object
-function retrieveUserData(patientID, key, onCall){
+function retrieveUserData(patientID, key, onCall) {
 
     var data = database.ref('Patients/' + patientID + '/' + key);
     data.on('value', onCall);
 }
 
-function getAllPatientNames(onCall){
+function getAllPatientNames(onCall) {
 
     return database.ref('Patients/').once('value').then(function(snapshot, onCall) {
         var val = snapshot.val();
 
         var label = [];
-        for(var i in val){
+        for (var i in val) {
             label.push(i);
         }
 
         var name = [];
-        for(var i = 0; i < label.length;i++) {
+        for (var i = 0; i < label.length; i++) {
             console.log(i);
             name.push(val[label[i]].Name);
         }
@@ -74,16 +74,16 @@ function getBasicPatientInfo() {
         var val = snapshot.val();
 
         var label = [];
-        for(var i in val){
+        for (var i in val) {
             label.push(i);
         }
 
         var list = [];
-        for(var i = 0; i < label.length;i++) {
+        for (var i = 0; i < label.length; i++) {
             console.log(i);
             var obj = {
-                'pid' : label[i],
-                'name' : val[label[i]].Name
+                'pid': label[i],
+                'name': val[label[i]].Name
             };
 
             list.push(obj);
